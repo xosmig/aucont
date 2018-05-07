758a7a7a6f62382bf2354b4fa7c0a03d291af535
Andrei Tonkikh <andrei.tonkikh@gmail.com>
2018-05-07 22:07:41 +0300

    Implement aucont_list

--- src/list/main.rs
+++ src/list/main.rs
@@ -1,4 +1,15 @@
 extern crate aucont;
 
+use std::fs;
+
 fn main() {
+    let dir = fs::read_dir("/tmp/aucont/containers")
+        .expect("Error opening directory");
+
+    for entry in dir {
+        let path = entry.expect("Error listing directory").path();
+        let cont_name_path = path.iter().last().expect("Error reading container id");
+        let cont_name_str = cont_name_path.to_str().expect("Error reading container id");
+        println!("{}", cont_name_str);
+    }
 }
